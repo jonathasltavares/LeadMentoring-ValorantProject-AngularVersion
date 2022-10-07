d8be90f5ade486b449cc57d5c07dd231d667ebb9
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-10-07 16:16:42 -0300

    adicionando sobreojogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { NoticiasComponent } from './noticias/noticias.component';
+import { SobreojogoComponent } from './sobreojogo/sobreojogo.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { NoticiasComponent } from './noticias/noticias.component';
     HeaderComponent,
     HomeComponent,
     NoticiasComponent,
+    SobreojogoComponent,
   ],
   imports: [
     BrowserModule,
