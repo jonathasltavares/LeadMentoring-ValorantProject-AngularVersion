fd53a624af68c9c53b5602dfeca348c3b69b1c74
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-10-10 16:06:40 -0300

    função logout

--- src/app/feature/components/header/header.component.ts
+++ src/app/feature/components/header/header.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  html = document.getElementsByTagName('html')[0]
+  normalSize = 16
+  currentSize = 16
   constructor() { }
 
   ngOnInit(): void {
@@ -15,4 +17,35 @@ export class HeaderComponent implements OnInit {
   scrollTo(element: any): void {
     (document.getElementById(element) as HTMLElement).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
+
+   logout(){
+      window.location.href = '/login'
+  }
+
+   incremetFontSize(){
+      if(this.currentSize<22){
+          this.currentSize+=2
+          this.setCurrentSize(this.currentSize)
+
+      }
+  }
+
+ 
+   decremetFontSize(){
+      if(this.currentSize>10){
+          this.currentSize-=2
+          this.setCurrentSize(this.currentSize)
+      }
+  }
+
+   resetFontSize(){
+      this.currentSize = this.normalSize
+      this.setCurrentSize(this.currentSize)
+  }
+
+   setCurrentSize(currentSize: any){
+      this.html.style.fontSize = `${currentSize}px`
+      console.log(this.currentSize)
+  }
+
 }
